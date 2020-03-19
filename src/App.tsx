567d1d78a71bab1d609344149848b6e5ffe7d922
Yevhen Chernenko <chernenko.eu@gmail.com>
2020-03-18 22:33:50 -0700

    Setting up Firebase.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,81 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import fire from "./fire";
+import { createGlobalStyle}  from "styled-components";
+
+const OMDbApiKey: string = '36827e98';
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  
+  body {
+  }
+`;
 
 function App() {
+  const [directorsLoaded, setDirectorsLoaded] = useState(false);
+  const [moviesLoaded, setMoviesLoaded] = useState(false);
 
-  async function getData() {
-    try {
-      //const { data } = await axios.get(`http://www.omdbapi.com/?t=goodfellas&y=1990&plot=full&apikey=36827e98`, {
-      const { data } = await axios.get(`http://www.omdbapi.com/?s=fincher&apikey=36827e98`, {
-      });
+  useEffect(() => {
+    fetchData();
+  },[]);
 
-      console.log(data);
-    } catch (e) {
-    }
-  }
+  const fetchData = () => {
+    fetchDirectors();
+    fetchMovies();
+  };
+
+  const fetchDirectors = () => {
+    fire.firestore().collection('directors').orderBy("name").onSnapshot((snapshot: any) => {
+      //dispatch({type: teamsReducer.actions.TEAMS_FETCH, snapshot: snapshot});
+      setDirectorsLoaded(true);
+    }, (error: { message: any; }) => {
+      console.log(error.message);
+    });
+  };
+
+  const fetchMovies = () => {
+    fire.firestore().collection('movies').orderBy("year").onSnapshot((snapshot: any) => {
+      //dispatch({type: teamsReducer.actions.TEAMS_FETCH, snapshot: snapshot});
+      setMoviesLoaded(true);
+    }, (error: { message: any; }) => {
+      console.log(error.message);
+    });
+  };
+
+  // async function getData() {
+  //   try {
+  //     const { data } = await axios.get(`http://www.omdbapi.com/?t=goodfellas&y=1990&plot=full&apikey=${OMDbApiKey}`, {
+  //     });
+  //
+  //     console.log(data);
+  //   } catch (e) {
+  //   }
+  // }
+
+  const content = (
+      <>
+        <span>Content</span>
+        <GlobalStyles/>
+      </>
+  );
+
+  const loader = (
+      <>
+        <span>Loading...</span>
+      </>
+  );
 
   return (
-    <div>
-      <button type='button' onClick={getData}>Goodfellas</button>
-    </div>
+    <>
+      {(directorsLoaded && moviesLoaded) ? content : loader}
+      {/*<button type='button' onClick={getData}>Goodfellas</button>*/}
+    </>
   );
 }
 
---
+++ src/fire.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAPqEWX9_Rf42-bVy_x0rYYRjkVRXmWZjE",
+    authDomain: "watcher-2932.firebaseapp.com",
+    databaseURL: "https://watcher-2932.firebaseio.com",
+    projectId: "watcher-2932",
+    storageBucket: "watcher-2932.appspot.com",
+    messagingSenderId: "445629515970",
+    appId: "1:445629515970:web:7022bb511ff8058bb9135b",
+    measurementId: "G-QN3767WMBQ"
+};
+
+const fire = firebase.initializeApp(firebaseConfig);
+export default fire;
